ff23da47698905cc032ff944c30d6844cb602bc0
snogcel <ledniknoj@gmail.com>
2016-02-28 01:07:55 -0700

    temporary hack - prevent version differences issue with bitcore-lib-dash

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+delete global._bitcore // hack -- due to version differences with dash bitcore and npm bitcore, prevents duplicate bitcore warning
 var bitcore = require('bitcore-lib-dash');
 bitcore.P2P = require('./lib');
 
