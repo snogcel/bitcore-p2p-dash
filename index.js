82952bfac12692019a22ffbf711255c6b5cbf93d
snogcel <ledniknoj@gmail.com>
2016-03-06 01:20:36 -0700

    update dependency name

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-delete global._bitcore // hack -- due to version differences with dash bitcore and npm bitcore, prevents duplicate bitcore warning
 var bitcore = require('bitcore-lib-dash');
 bitcore.P2P = require('./lib');
 
